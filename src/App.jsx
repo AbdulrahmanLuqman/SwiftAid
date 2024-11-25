1fb904c8306bcd7d58fb6486d177e1c820e3a0fb
AbdulrahmanLuqman <luqmanola60@gmail.com>
2024-11-25 02:04:22 +0100

    configured router and page load

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import OnBoarding from "./views/OnBoarding";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(()=> {
-    const handleIsLoading = ()=> {
-      setIsLoading(false)
-    }
-
-    window.addEventListener("load", handleIsLoading)
-    
-
-    return ()=> {
-      window.removeEventListener("load", handleIsLoading)
-    }
-  }, [])
   return (
     <>
-      {
-        isLoading ? <div>Loading...</div> : 
         <main>
           <Routes>
             <Route path="/" element={<OnBoarding />} />
           </Routes>
         </main>
-      }
     </>
   )
 }
--- src/views/OnBoarding.jsx
+++ src/views/OnBoarding.jsx
@@ -1,8 +1,29 @@
-
+import { useState, useEffect } from "react";
 const OnBoarding = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(()=> {
+        if(document.readyState === 'complete'){
+            setIsLoading(false)
+        } else {
+            const handleIsLoading = ()=> {
+                setIsLoading(false)
+            }
+
+            window.addEventListener("load", handleIsLoading)
+
+            return ()=> {
+                window.removeEventListener("load", handleIsLoading)
+            }
+        }
+    
+    }, [])
+    // console.log(document.readyState)
   return (
     <div>
-
+        {
+             isLoading ? <div>Loading...</div> : <div>Page Loaded</div>
+        }
     </div>
   )
 }
